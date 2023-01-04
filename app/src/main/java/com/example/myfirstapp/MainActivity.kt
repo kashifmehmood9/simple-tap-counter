758104f7136f708ced615f08189e5342284e00da
kashifmehmood9 <econ.mehmoodkashif@gmail.com>
2023-01-04 14:26:13 +0800

    Added counter, button and textview

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.mybutton)
+        val textView = findViewById<TextView>(R.id.textView)
+        var timesClicked = 0
+        button.setOnClickListener {
+            timesClicked++
+            textView.text =  "$timesClicked"
+            Toast.makeText(this,"Button Tapped", Toast.LENGTH_SHORT).show()
+        }
     }
 }
